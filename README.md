3db0711a6d1b4f42984031eb2fd1b1e811fa6e07
jlin95 <jlin95@users.noreply.github.com>
2024-04-23 22:23:12 +0000

    Solutions to week 3

---
+++ README.md
@@ -0,0 +1,5 @@
+## Potential Solutions to Avoid Toil
+
+One way to fix recurring obsolete alerts is to set up a reminder alert to the SRE team to review those obsolete alerts, with thresholds set in terms of weeks or months.
+
+For lack of prioritization, a recurring meeting could be useful to set priorities for issues. 
--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,20 @@
-#TODO
+#!/bin/bash
+
+NAMESPACE="sre"
+DEPLOYMENT="swype-app"
+
+MAX_RESTARTS=4
+
+while true; do 
+    RESTARTS=$(kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT} -o jsonpath="{.items[0]}.status.containerStatuses[0].restartCount"})
+
+    echo "Current no. of restarts: ${RESTARTS}"
+
+    if (( RESTARTS > MAX_RESTARTS )); then 
+        echo "Max number of restarts hit, scaling down deployment"
+        kubectl scale --replicas=0 deployment/${DEPLOYMENT} -n ${NAMESPACE}
+        break
+    fi
+
+    sleep 60
+done
